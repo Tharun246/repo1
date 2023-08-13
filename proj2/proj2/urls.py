249c43e3b7c5275868b5a040adbfebf737fe1445
Tharun <theunstoppable992@gmail.com>
2023-08-13 18:36:20 +0530

    initial

--- proj2/proj2/urls.py
+++ proj2/proj2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project2 project.
+URL configuration for proj2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
